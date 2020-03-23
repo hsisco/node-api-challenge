4a2e54e5c6fe448c5b7c97bac57133d6bd85f0ef
hsisco <hysensisco@gmail.com>
2020-03-22 18:38:53 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const server = express();
+
+const actionRouter = require('./.js');
+const projectRouter = require('./.js');
+
+server.use(express.json(), helmet(), morgan('dev'), logger);
+
+server.use('/actions', actionRouter);
+server.use('/projects', projectRouter);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Hysen's Attempt at Backend 🥴</h2>`);
+});
+
+function logger(req, res, next) {
+  const method = req.method;
+  const url = req.originalUrl;
+  const timestamp = new Date();
+
+  console.log(`A ${method} request to ${url} occurred at ${timestamp}.`);
+  next();
+}
+
+module.exports = server;
