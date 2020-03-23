86924b803f1ed0e5d90b4bdb813acd039f9acd9f
hsisco <hysensisco@gmail.com>
2020-03-22 18:13:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+require('dotenv').config();
+
+const server = require('./server.js');
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+	console.log(`\n*^^*~~Server Running on http://localhost:${PORT}~~*^^*\n`)
+});
